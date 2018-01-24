c6e9bf1a184a9a1e4ee20a38bbfeb408a75c5051
夜伴钟声 <hzschn@sina.com>
2018-01-24 15:44:46 +0800

    增加过滤emoji表情方法

--- string.php
+++ string.php
@@ -1,8 +1,8 @@
 <?php
     /*
-    *function：检测字符串是否由纯英文，纯中文，中英文混合组成
-    *param $str string
-    *return en:纯英文;cn:纯中文;encn:中英文混合
+    *@function：检测字符串是否由纯英文，纯中文，中英文混合组成
+    *@param $str string
+    *@return en:纯英文;cn:纯中文;encn:中英文混合
     */
     function ischinese($str){
         if(trim($str)==''){
@@ -19,9 +19,9 @@
         return 'encn';
     }
     /*
-    *function：检测字符串中文长度
-    *param $str string
-    *return $count int
+    *@function：检测字符串中文长度
+    *@param $str string
+    *@return $count int
     */
     function chineseCount($str){
         $pa = '/[\x{4e00}-\x{9fa5}]/siu';
@@ -30,14 +30,22 @@
         return $count;
     }
     /*
-    *function：检测字符串长度,中文算1个长度
-    *param $str string
-    *return int
+    *@function：检测字符串长度,中文算1个长度
+    *@param $str string
+    *@return int
     */
     function utf8_strlen($string = null) {
         // 将字符串分解为单元
         preg_match_all("/./us", $string, $match);
         // 返回单元个数
         return count($match[0]);
+    }
+    /*
+     * @function 过滤emoji表情
+     * @param $str string
+     * @return string
+     * */
+    function filterEmoji($str){
+        return preg_replace('/[\x{10000}-\x{10FFFF}]/u', '', $str);
     }
 ?>
